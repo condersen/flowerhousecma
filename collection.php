58fe106df863e12072ee9ecbe66d749a842887dc
connor andersen <condersen@gmail.com>
2014-10-14 14:05:14 -0700

    first addition

---
+++ collection.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Flower House | Home</title>
+		<link rel="stylesheet" href="css/flowerhousecss.css">
+		</head>
+		<div class="top-bar"></div>
+	<body>
+		<div id="layout-container">
+			<div id="header-container">
+				<div class="logo-container"></div>
+				<div class="nav-container">
+					<ul class="list-clean list-left nav-list">
+						<li><a href="index.php">Home</a></li>
+						<li class="special-color"><a href="index.php">Flower Collection</a></li>
+						<li><a href="index.php"><img src="img/phone.png">(949) 300 2565</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="content-container">
+				<div class="gallery">
+					<div class="carousel-picture img1"></div>
+					<div class="carousel-picture img2"></div>
+					<div class="carousel-picture img3"></div>
+				</div>
+				<div class="text-content">
+					<p>
+						We offer a wide range of flower arrangements fit for any occasion. We specialize in traditional, modern, and extravagant flower designs. Flower House guarantees the most beautiful, elegant, and fresh flower arrangements in town! Give us a call and our friendly customer service representative will be there to answer your questions and/or take your order.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="footer-text" id="footer-container">
+				<p>Copyright  2014 FlowerHouse. All rights reserved | 234 Terry Francois St. San Francisco, CA 94158, info@flowerhouse.com | Mon-Fri: 7am-10pm, Sat-8am-10pm</p>
+		</div>
+	</body>
+</html>
